8a924a3c1dbd02bef6c77bbe9da8b076cb396ff5
Shreyash <96154728+ShreyashJay@users.noreply.github.com>
2022-01-27 14:49:56 +0530

    Add files via upload

---
+++ conserved_codes.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <stdlib.h>  // rand is one of the constituents,don't know much
+# include <time.h>    // for time,don't know much
+# include <math.h>
+
+int main()
+{
+
+ /*    string with spaces
+    char name[100];
+    scanf("%[^\n]s",name);
+    printf("The string is: %s ",name);    */
+
+
+    /*  generate random int between two numbers
+int a,b,c,r;
+srand(time(NULL));
+for(b=0;b<5;b++){   r=rand()%46+5 ;   printf("%d \n",r);};
+// rand() generates random number from 0 to some upper limit
+// rand()%6 generates random number from 0 to 5
+// rand()%6+4 generates random number from (0+4) to (5+4)
+*/
+
+
+return 0;}
